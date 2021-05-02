07ed1bd367ca20ecdadd02a39e14963381f94a7e
KayhanTejani <kayhantejani@hotmail.com>
2021-05-03 01:21:25 +0500

    Added conditions to handle errors in input

--- main.js
+++ main.js
@@ -9,17 +9,28 @@ converts them into their phonetic equivalent and prints the result in standard o
 let values = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 
 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten"};
 
-// Reads arguments passed into command line and stores the integers to be converted in an array
-let args = process.argv.slice(2);
 
-// This variable will be updated to store the phonetic equivalents during execution
+// Reads arguments passed into command-line and stores input in array
+let args = process.argv.slice(2);
 let phonetic = "";
 
-intToPhonetic(args);
+
+// If input is not empty, execute main function
+if (args.length > 0) {
+    intToPhonetic(args);
+}
+else {
+    console.log("No input has been provided!");
+    return;
+}
 
 
+// Function that converts digits to phonetic equivalents
 function intToPhonetic(numInput) {
     for (let i = 0; i < numInput.length; i++) {
+        // If input is not a number, ignore it
+        if (isNaN(numInput[i]))
+            continue;
         if (numInput[i].length == 1 && numInput[i] in values) {
             phonetic += values[numInput[i]];
         }
@@ -33,6 +44,8 @@ function intToPhonetic(numInput) {
     }
 }
 
+
+// Helper function called to convert multi-digit numbers
 function convertLargeNum(num) {
     let largeNumber = num.split("");
     for (let j = 0; j < largeNumber.length; j++) {
